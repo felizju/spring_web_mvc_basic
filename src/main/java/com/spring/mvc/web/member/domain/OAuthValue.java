7e0f57548f7573a55b358dbba68ea5dca2b2bfe7
felizju1112 <felizju1112@gmail.com>
2021-06-11 16:00:32 +0900

    [21.06.11] logIn API 기능 구현

---
+++ src/main/java/com/spring/mvc/web/member/domain/OAuthValue.java
@@ -0,0 +1,8 @@
+package com.spring.mvc.web.member.domain;
+
+//소셜로그인 관련 상수 모음
+public interface OAuthValue {
+    String KAKAO_APP_KEY = "2c329a02e7cbdb3ae60ee6da3e90e66b";
+    String KAKAO_REDIRECT_URI = "/auth/kakao";
+
+}
